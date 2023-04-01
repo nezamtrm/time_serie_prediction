4c4405af818ddc71f47d918de13b6f754351a8f6
nezamtrm <128442592+nezamtrm@users.noreply.github.com>
2023-04-01 06:31:57 +0330

    add comments

--- gen_data.py
+++ gen_data.py
@@ -2,24 +2,7 @@ import pandas as pd
 from sklearn import preprocessing
 import numpy as np
 
-# def generate_data(data_x, data_y, indx, win_size):
-#     x, y = [], []
-#     for i in range(win_size, indx):
-#         x.append(data_x.iloc[i - win_size:i, :])
-#         y.append(data_y.iloc[i])
-#         return np.array(x), np.array(y)
-
-
-# def generate_xdata(data_x, index, win_size):
-#     x = []
-#     for i in range(win_size, index):
-#         x.append(data_x.iloc[i-win_size:i,:])
-#     return np.array(x)
-# def generate_ydata(data_y, index, win_size):
-#     y = []
-#     for i in range(win_size, index):
-#         y.append(data_y.iloc[i])
-#     return np.array(y)
+#The following function generates windows of input data and corresponding output data for a given window size and index position:
 def generate_data(data_x, data_y, index, win_size):
     x, y = [], []
     for i in range(win_size, index):
@@ -27,6 +10,7 @@ def generate_data(data_x, data_y, index, win_size):
         y.append(data_y.iloc[i])
     return np.array(x), np.array(y)
 
+#The following function scales input data using StandardScaler from scikit-learn library and returns both the scaler and the scaled data:
 def preprcss_data(data):
     scaler = preprocessing.StandardScaler().fit(data)
     data_scaled = scaler.transform(data) #returns np array
